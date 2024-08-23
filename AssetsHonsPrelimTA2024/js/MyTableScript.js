17873e36460d165df85d71691d8b84aaa4ce3aae
drcgb <cg.bean@outlook.com>
2024-08-24 01:12:30 +0930

    Update MyTableScript.js

--- AssetsHonsPrelimTA2024/js/MyTableScript.js
+++ AssetsHonsPrelimTA2024/js/MyTableScript.js
@@ -212,12 +212,12 @@ document.addEventListener("DOMContentLoaded", async () => {
         const methodFilter = document.getElementById("methodFilter");
         methodFilter.innerHTML = `
             <option value="" style="font-weight: bold;">All Methods</option>
-            <optgroup label="<strong>[Quantitative]</strong>" class="optgroup-bold">
+            <optgroup label="[Quantitative]" class="optgroup-bold">
                 <option value="all-quantitative">&#x2192; ALL Quantitative [≈${methodCounts.quantitative + methodCounts.metaAnalysis + methodCounts.mixedMethodsQuantitative} records]</option>
                 <option value="meta-analysis">&nbsp;&nbsp;&nbsp;&#x2198; Meta-Analysis [≈${methodCounts.metaAnalysis} records]</option>
                 <option value="mixed-methods-quantitative">&nbsp;&nbsp;&nbsp;&#x2198; Mixed-Methods [≈${methodCounts.mixedMethodsQuantitative} records]</option>
             </optgroup>
-                <optgroup label="<strong>[Qualitative]</strong>" class="optgroup-bold">
+                <optgroup label="[Qualitative]" class="optgroup-bold">
                 <option value="all-qualitative">&#x2192; ALL Qualitative [≈${methodCounts.qualitative + methodCounts.metaSynthesis + methodCounts.mixedMethodsQualitative} records]</option>
                 <option value="meta-synthesis">&nbsp;&nbsp;&nbsp;&#x2198; Meta-Synthesis [≈${methodCounts.metaSynthesis} records]</option>
                 <option value="mixed-methods-qualitative">&nbsp;&nbsp;&nbsp;&#x2198; Mixed-Methods [≈${methodCounts.mixedMethodsQualitative} records]</option>
