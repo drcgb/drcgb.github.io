8d62711b0b3e6aea80857048614fa96a0ca0c309
drcgb <cg.bean@outlook.com>
2024-08-25 21:55:44 +0930

    Update scriptD.js

--- AssetsHonsPrelimTA2024/js/scriptD.js
+++ AssetsHonsPrelimTA2024/js/scriptD.js
@@ -256,7 +256,7 @@ function adjustContentMargin() {
     // Calculate total margin only considering visible elements
     const totalMargin = headerHeight + (filterNoticeHeight > 0 ? filterNoticeHeight : 0);
 
-    $('.content').css('margin-top', totalMargin - 10);
+    $('.content').css('margin-top', totalMargin - 1);
 }
 
 function matchNoticeWidth() {
