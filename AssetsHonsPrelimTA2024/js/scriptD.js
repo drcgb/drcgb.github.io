8c57a390abd7821bd26ef783fd309b6f7e505078
drcgb <cg.bean@outlook.com>
2024-08-26 03:18:50 +0930

    Update scriptD.js

--- AssetsHonsPrelimTA2024/js/scriptD.js
+++ AssetsHonsPrelimTA2024/js/scriptD.js
@@ -257,7 +257,8 @@ function updateFilterNotice() {
 function adjustContentMargin() {
     const filterNoticeHeight = $('#filterNotice').is(':visible') ? $('#filterNotice').outerHeight(true) : 0;
     const instructionsHeight = $('#instructionsDetails').is(':visible') && $('#instructionsDetails').attr('open') ? $('#instructionsDetails').outerHeight(true) : 0;
-    const headerHeight = $('.fixed-header').outerHeight(true) + 15;
+    const blueBarHeight = $('.blue-bar').outerHeight(true); // Get the height of the blue bar
+    const headerHeight = $('.fixed-header').outerHeight(true) + blueBarHeight;
 
     // Adjust the total margin so that it only adds the filter notice height if needed
     const totalMargin = headerHeight + filterNoticeHeight + instructionsHeight;
